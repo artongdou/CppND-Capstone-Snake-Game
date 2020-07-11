23dce81faa9885b1820dd6ba53bf375ffee9e4c5
artongdou <cstong@umich.edu>
2020-07-11 19:49:22 -0400

    templated messagebox class

--- src/message.cpp
+++ src/message.cpp
@@ -2,6 +2,31 @@
 
 #include <iostream>
 
+EscapeMessageBox::EscapeMessageBox() {
+  _boxData = {
+      SDL_MESSAGEBOX_INFORMATION, /* .flags */
+      NULL,                       /* .window */
+      "Quit/Restart",             /* .title */
+      "select a button",          /* .message */
+      SDL_arraysize(_buttonData), /* .numbuttons */
+      _buttonData,                /* .buttons */
+      &_colorScheme               /* .colorScheme */
+  };
+  _colorScheme = {{/* .colors (.r, .g, .b) */
+                   /* [SDL_MESSAGEBOX_COLOR_BACKGROUND] */
+                   {255, 0, 0},
+                   /* [SDL_MESSAGEBOX_COLOR_TEXT] */
+                   {0, 255, 0},
+                   /* [SDL_MESSAGEBOX_COLOR_BUTTON_BORDER] */
+                   {255, 255, 0},
+                   /* [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] */
+                   {0, 0, 255},
+                   /* [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] */
+                   {255, 0, 255}}};
+  _buttonData[0] = {SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 0, "End"};
+  _buttonData[1] = {SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 1, "Restart"};
+}
+
 int EscapeMessageBox::Show() const {
   int button_id;
   if (SDL_ShowMessageBox(&_boxData, &button_id) < 0) {
--- src/message.h
+++ src/message.h
@@ -2,43 +2,54 @@
 #define message_h
 
 #include <memory>
+#include <vector>
 
 #include "SDL.h"
 
+template <unsigned size>
 class MessageBox {
  public:
   virtual int Show() const = 0;
+  SDL_MessageBoxData _boxData;
+  SDL_MessageBoxColorScheme _colorScheme;
+  SDL_MessageBoxButtonData _buttonData[size];
 };
 
-class EscapeMessageBox : public MessageBox {
+class EscapeMessageBox : public MessageBox<2> {
  public:
-    int Show() const override;
- private:
-  const SDL_MessageBoxData _boxData = {
-      SDL_MESSAGEBOX_INFORMATION, /* .flags */
-      NULL,                       /* .window */
-      "Quit/Restart",             /* .title */
-      "select a button",          /* .message */
-      SDL_arraysize(_buttonData), /* .numbuttons */
-      _buttonData,                /* .buttons */
-      &_colorScheme               /* .colorScheme */
-  };
-  const SDL_MessageBoxColorScheme _colorScheme = {
-      {/* .colors (.r, .g, .b) */
-       /* [SDL_MESSAGEBOX_COLOR_BACKGROUND] */
-       {255, 0, 0},
-       /* [SDL_MESSAGEBOX_COLOR_TEXT] */
-       {0, 255, 0},
-       /* [SDL_MESSAGEBOX_COLOR_BUTTON_BORDER] */
-       {255, 255, 0},
-       /* [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] */
-       {0, 0, 255},
-       /* [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] */
-       {255, 0, 255}}};
-  const SDL_MessageBoxButtonData _buttonData[2] = {
-      {SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 0, "End"},
-      {SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 1, "Restart"},
-  };
+   EscapeMessageBox();
+  int Show() const override;
 };
+
+// class EscapeMessageBox : public MessageBox {
+//  public:
+//     int Show() const override;
+//  private:
+//   const SDL_MessageBoxData _boxData = {
+//       SDL_MESSAGEBOX_INFORMATION, /* .flags */
+//       NULL,                       /* .window */
+//       "Quit/Restart",             /* .title */
+//       "select a button",          /* .message */
+//       SDL_arraysize(_buttonData), /* .numbuttons */
+//       _buttonData,                /* .buttons */
+//       &_colorScheme               /* .colorScheme */
+//   };
+//   const SDL_MessageBoxColorScheme _colorScheme = {
+//       {/* .colors (.r, .g, .b) */
+//        /* [SDL_MESSAGEBOX_COLOR_BACKGROUND] */
+//        {255, 0, 0},
+//        /* [SDL_MESSAGEBOX_COLOR_TEXT] */
+//        {0, 255, 0},
+//        /* [SDL_MESSAGEBOX_COLOR_BUTTON_BORDER] */
+//        {255, 255, 0},
+//        /* [SDL_MESSAGEBOX_COLOR_BUTTON_BACKGROUND] */
+//        {0, 0, 255},
+//        /* [SDL_MESSAGEBOX_COLOR_BUTTON_SELECTED] */
+//        {255, 0, 255}}};
+//   const SDL_MessageBoxButtonData _buttonData[2] = {
+//       {SDL_MESSAGEBOX_BUTTON_RETURNKEY_DEFAULT, 0, "End"},
+//       {SDL_MESSAGEBOX_BUTTON_ESCAPEKEY_DEFAULT, 1, "Restart"},
+//   };
+// };
 
 #endif
